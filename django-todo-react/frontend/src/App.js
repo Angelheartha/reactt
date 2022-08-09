b930a8e49618f4993b7d5c0d9086a87eec8684c2
John Smith <aisuruyamano@gamail.com>
2022-08-09 19:13:24 +0900

    js

--- django-todo-react/frontend/src/App.js
+++ django-todo-react/frontend/src/App.js
@@ -1,41 +1,10 @@
-import React, { useState} from "react";
+
+import React, { Component, useState } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
-import { useAlert } from 'react-alert'
 
 
-
-const App = () => {
-  const [viewCompleted, setViewCompleted] = useState(false);
-  const [todoList, setTodoList] = useState([]);
-  const [modal, setModal] = useState(false);
-  const [how, setHow] = useState("");
-  const [what, setWhat] = useState("");
-  const [activeItem, setActiveItem] = useState({title: "", description: "", completed: false });
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [description,setDescription] = useState("");
-  const alert = useAlert()
-  const [input, setInput]=useState("");
-  const [textarea, setTextarea]=useState("");
-
-
-
-  const componentDidMount = (props) => {
-    props.refreshList();
-  };
-
-  const refreshList = (props) => {
-    axios
-      .get("http://localhost:8000/api/todos/")
-      .then((res) => setTodoList({ todoList: res.data }))
-      .catch((err) => console.log(err));
-  };
-
-
-
-const Act = (props) => {
- const alert = useAlert()
-
+function Act(props) {
   return (
      <div id="overlay">
          <div id="modal" className="modall">
@@ -45,12 +14,12 @@ const Act = (props) => {
           <input />
           <p>Message</p>
           <textarea />
+          <input
+            type='submit'
+            value='submit'
+          />
           <p>I am available at this moment!</p>
         <button onClick={props.onClick}>Close</button>
-        <button type="button" onClick={aalert}>
-        Submit
-        </button>
-
         </form>
 
       </div>
@@ -60,73 +29,134 @@ const Act = (props) => {
   )
 }
 
-  const aalert = () => {
-  alert.show('your message is already sent!! i reply in 5 days bussine daysss');
-  setInput([]);
-  setTextarea([]);
+
+class App extends Component{
+  ///const[viewCompleted]=useState(false)
+  ///const[todoList]=useState([])
+  ///const[modal]=useState(false)
+  ///const[how]=useState("")
+  ///const[what]=useState("")
+  ///const[activeItem]=useState([{title: "",description: "",completed: false}])
+  ///const[isSubmitted]=useState(false)
+  ///const[email]=useState("sample@gmail.com")
+  ///const[hasEmailError]=useState(false)
+  ///const[content]=useState("お問い合わせ内容")
+  ///const[hasContactError]=useState(false)
+  ///const[isModalOpen,setIsModalOpen]=useState(false)
+  ///const[isModalOpen,setIsModalOpen]=useState(false)
+  ///const[isModalOpen,setIsModalOpen]=useState(false)
+  ///const[isModalOpen,setIsModalOpen]=useState(false)
+  ///const[isModalOpen,setIsModalOpen]=useState(false)
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      todoList: [],
+      modal: false,
+      how: "",
+      what: "",
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false,
+      },
+      isSubmitted: false,
+      email: "sample@gmail.com",
+      hasEmailError: false,
+      content: "お問い合わせ内容",
+      hasContactError: false,
+    };
+    this.howtodo = this.howtodo.bind(this);
+    this.whattodo = this.whattodo.bind(this);
+    this.props = {isModalOpen: false};
+    this.props = {setIsModalOpen:false};
+  }
+
+
+
+
+
+
+    whattodo() {
+    this.setState({ how: "You can write any recommendations for language studies on the Description. anything is ok" });
+  }
+
+
+
+
+
+
+
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/todos/")
+      .then((res) => this.setState({ todoList: res.data }))
+      .catch((err) => console.log(err));
   };
 
-
-
-  const toggle = () => {
-    setModal(!modal);
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
   };
 
+  handleSubmit = (item) => {
+    this.toggle();
 
-
-  const handleSubmit = (item, props) => {
-    setModal(!modal);
-
-        if (item.id) {
-       axios
-         .put(`http://localhost:8000/api/todos/${item.id}/`, item)
-         .then(() => props.refreshList());
-       return;
-     }
-     axios
-       .post("http://localhost:8000/api/todos/", item)
-       .then(() => refreshList());
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    axios
+      .post("http://localhost:8000/api/todos/", item)
+      .then((res) => this.refreshList());
   };
 
-   const handleDelete = (item,props) => {
+  handleDelete = (item) => {
     axios
       .delete(`http://localhost:8000/api/todos/${item.id}/`)
-      .then((res) => props.refreshList());
+      .then((res) => this.refreshList());
   };
 
-
-  const createItem = (props) => {
+  createItem = () => {
     const item = { title: "", description: "", completed: false };
-    setActiveItem(activeItem, item);
-    setModal(!modal);
+
+    this.setState({ activeItem: item, modal: !this.state.modal });
   };
 
-  const editItem = (props,item) => {
-    setActiveItem(activeItem, item);
-    setModal(!modal);
+  editItem = (item) => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
   };
 
-  const displayCompleted = (status,props) => {
+  displayCompleted = (status) => {
     if (status) {
-      return setViewCompleted({ viewCompleted: true });
+      return this.setState({ viewCompleted: true });
     }
 
-      return setViewCompleted({ viewCompleted: false });
+    return this.setState({ viewCompleted: false });
   };
 
 
-  const renderTabList = () => {
+
+
+  renderTabList = () => {
     return (
       <div className="nav nav-tabs">
         <span
-          onClick={() => setViewCompleted(true)}
-          className={viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
         >
           Complete
         </span>
         <span
-          onClick={() => setViewCompleted(false)}
-          className={viewCompleted ? "nav-link" : "nav-link active"}
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
         >
           Incomplete
         </span>
@@ -134,121 +164,130 @@ const Act = (props) => {
     );
   };
 
-  const renderItems = (props) => {
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+      (item) => item.completed === viewCompleted
+    );
 
-  const newItems = todoList.filter(
-       (item) => item.completed === viewCompleted
-     );
 
-     return newItems.map((item, props) => (
-       <li
-         key={item.id}
-         className="list-group-item d-flex justify-content-between align-items-center"
-       >
-         <span
-           className={`todo-title mr-2 ${viewCompleted ? "completed-todo" : ""
-             }`}
-           title={item.description}
-         >
-           {item.title}
-         </span>
-         <span>
-           <button
-             className="btn btn-secondary mr-2"
-             onClick={() => props.editItem(item)}
-           >
-             Edit
-           </button>
-           <button
-             className="btn btn-danger"
-             onClick={() => props.handleDelete(item)}
-           >
-             Delete
-           </button>
-         </span>
-       </li>
-     ));
+
+
+
+
+
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""
+            }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            className="btn btn-secondary mr-2"
+            onClick={() => this.editItem(item)}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.handleDelete(item)}
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
   };
 
-  const whattodo = () => {
-      setDescription("You can write any recommendations for language studies on the Description. anything is ok");
-  }
+
+  render() {
+    return (
+      <main className="container">
+        <div className="nav-container">
+          <button
+            className="navi white How btn btn-primary"
+            onClick={()=>{this.howtodo()}}
+          >
+            How to use
+          </button>
+
+          <button
+            className="navi What btn btn-primary"
+            onClick={()=>{this.whattodo()}}
+          >
+            What to write
+          </button>
 
 
-  const howtodo = () => {
-    setDescription("You should write the day and to whom you write on the Title.⇒(ex).title:7/7 for Hamuster");
-  }
+          <button
+            className="navi contact btn btn-primary"
+            onClick={() => { this.openModal()}}
+          >
+            Contact
+          </button>
+        </div>
 
-
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
-
-
-  const openModal = () => {
-    setIsModalOpen(true)
-  }
-
-
-  return (
-    <main className="container">
-      <div className="nav-container">
-
-
-        <button
-          className="navi white How btn btn-primary"
-          onClick={howtodo}
-        >
-          How to use
-        </button>
-
-        <button
-          className="navi What btn btn-primary"
-          onClick={whattodo}
-        >
-          What to write
-        </button>
-
-
-        <button
-          className="navi contact btn btn-primary"
-          onClick={() => { openModal() }}
-        >
-          Contact
-        </button>
-      </div>
-
-      {isModalOpen && <Act onClick={() => { closeModal() }} />}
-      <p className="howtitle">{description}</p>
-      <h1 className="text-uppercase text-center my-4">Todo app</h1>
-      <div className="row">
-        <div className="col-md-6 col-sm-10 mx-auto p-0">
-          <div className="card p-3">
-            <div className="mb-4">
-              <button
-                className="btn btn-primary"
-                onClick={createItem}
-              >
-                Add
-              </button>
+        {this.state.isModalOpen ? <Act onClick={() => { this.closeModal()}} /> : ""}
+        <p className="howtitle">{this.state.how}</p>
+        <p className="howtitle">{this.state.what}</p>
+        <h1 className="text-uppercase text-center my-4">Todo app</h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button
+                  className="btn btn-primary"
+                  onClick={this.createItem}
+                >
+                  Add
+                </button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
             </div>
-            {renderTabList()}
-            <ul className="list-group list-group-flush border-top-0">
-              {renderItems()}
-            </ul>
           </div>
         </div>
-      </div>
-      {modal && (
-        <Modal
-          activeItem={activeItem}
-          toggle={toggle}
-          onSave={handleSubmit}
-        />
-      )}
-    </main>
-  )
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
 
+
+
+
+      </main>
+    );
+  }
+  howtodo() {
+    this.setState({ how: "You should write the day and to whom you write on the Title.⇒(ex).title:7/7 for Hamuster" });
+  }
+
+
+  closeModal() {
+    this.setState({isModalOpen: false})
+  }
+
+
+  openModal() {
+    this.setState({isModalOpen: true})
+  }
+
+  handleSubmit(){
+    this.setState({isSubmitted:true})
+  }
 
 
 
